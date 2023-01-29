961717b6e716390527d08eb28485bebe7aa39dbf
anthony <a.bagley.dev@outlook.com>
2023-01-29 16:09:45 +0000

    Use stable PunkApiNet library

--- src/BlazorPunkApi/Program.cs
+++ src/BlazorPunkApi/Program.cs
@@ -1,6 +1,6 @@
-using BlazorPunkApi;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorPunkApi;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
