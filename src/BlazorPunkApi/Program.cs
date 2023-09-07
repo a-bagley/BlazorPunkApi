5c07d73dc10e7a5dce73ad6cd178698f270143ed
Anthony Bagley <a.bagley.dev@outlook.com>
2023-09-07 22:25:49 +0100

    Beers prototype (#4)
    * Quick POC with PunkApi client library
    
    * Use stable PunkApiNet library
    
    * Minor additions

--- src/BlazorPunkApi/Program.cs
+++ src/BlazorPunkApi/Program.cs
@@ -1,11 +1,13 @@
-using BlazorPunkApi;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorPunkApi;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddPunkApiClient();
 
 await builder.Build().RunAsync();
